38176cc2cbc24ebda262f45171ac4447459658fd
anthonylin <anthonylin@fih-foxconn.com>
2018-09-05 13:29:26 +0800

    According suggest, modify contract context

--- ioeX_v01.sol
+++ ioeX_v01.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.21;
+pragma solidity 0.4.24;
 
 // ----------------------------------------------------------------------------
 //
@@ -11,51 +11,7 @@ pragma solidity 0.4.21;
 // ----------------------------------------------------------------------------
 
 
-library SafeMath {
-    /**
-    * @dev Multiplies two numbers, throws on overflow.
-    */
-    function mul(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
-        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (_a == 0) {
-            return 0;
-        }
-
-        c = _a * _b;
-        assert(c / _a == _b);
-        return c;
-    }
-
-    /**
-    * @dev Integer division of two numbers, truncating the quotient.
-    */
-    function div(uint256 _a, uint256 _b) internal pure returns (uint256) {
-        // assert(_b > 0); // Solidity automatically throws when dividing by 0
-        // uint256 c = _a / _b;
-        // assert(_a == _b * c + _a % _b); // There is no case in which this doesn't hold
-        return _a / _b;
-    }
-
-    /**
-    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-    */
-    function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
-        assert(_b <= _a);
-        return _a - _b;
-    }
-
-    /**
-    * @dev Adds two numbers, throws on overflow.
-    */
-    function add(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
-        c = _a + _b;
-        assert(c >= _a);
-        return c;
-    }
-}
-
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract owned {
     address public owner;
@@ -64,7 +20,7 @@ contract owned {
         address indexed newOwner
     );
 
-    function owned() public {
+    constructor() public {
         owner = msg.sender;
     }
 
@@ -99,9 +55,9 @@ contract ERC20 {
 contract StandardToken is ERC20 {
     using SafeMath for uint256;
 
-    mapping(address => uint256) balances;
+    mapping(address => uint256) internal balances;
 
-    uint256 totalSupply_;
+    uint256 internal totalSupply_;
 
     /**
      * @dev Total number of tokens in existence
@@ -140,18 +96,16 @@ contract ioeXTokenERC20 is StandardToken, owned {
     using SafeMath for uint256;
 
     // Public variables of the token
-    string public name = "Internet of Everything X";
+    bytes internal name_ = "Internet of Everything X";
 
-    string public symbol = "IOEX";
+    bytes internal symbol_ = "IOEX";
 
     uint256 public decimals = 8;
 
-    uint constant LOCK_TYPE_MAX = 3;
-    uint constant LOCK_STAGE_MAX = 4;
+    uint256 private constant LOCK_TYPE_MAX = 3;
+    uint256 private constant LOCK_STAGE_MAX = 4;
 
     mapping (address => bool) public frozenAccount;
-
-    mapping (address => StructLockAccountInfo) public lockAccountInfo;
 
     //Save lock type and amount of init tokens
     struct StructLockAccountInfo {
@@ -159,14 +113,16 @@ contract ioeXTokenERC20 is StandardToken, owned {
         uint256 initBalance;
         uint256 startTime;
     }
+
+    mapping (address => StructLockAccountInfo) public lockAccountInfo;
  
     //Save 4 set of time and percent of unlocked tokens
     struct StructLockType {
         uint256[LOCK_STAGE_MAX] time;
         uint256[LOCK_STAGE_MAX] freePercent;
-    } 
+    }
 
-    StructLockType[LOCK_TYPE_MAX] lockType;
+    StructLockType[LOCK_TYPE_MAX] private lockType;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -176,7 +132,7 @@ contract ioeXTokenERC20 is StandardToken, owned {
 
     // This generates a public event that record info about locked account,
     // including amount of init tokens and lock type
-    event SetlockData(address indexed account, uint256 initBalance, uint256 lock_type, uint256 startDate);
+    event SetLockData(address indexed account, uint256 initBalance, uint256 lockType, uint256 startDate);
 
     /* This generates a public event on the blockchain that will notify clients */
     event FrozenFunds(address target, bool frozen);
@@ -186,44 +142,60 @@ contract ioeXTokenERC20 is StandardToken, owned {
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    function ioeXTokenERC20() public {
-        totalSupply_ = 200000000 * 10 ** uint256(decimals);
+    constructor() public {
+        totalSupply_ = 20000000000000000;
         balances[msg.sender] = totalSupply_;  // Give the creator all initial tokens
 
         //init all lock data
-        //Lock type 1        
-        lockType[0].time[0] = 30;          
+        //Lock type 1
+        lockType[0].time[0] = 30;
         lockType[0].freePercent[0] = 40;     //40%
-        lockType[0].time[1] = 60;    
+        lockType[0].time[1] = 60;
         lockType[0].freePercent[1] = 20;     //20%
-        lockType[0].time[2] = 120;    
+        lockType[0].time[2] = 120;
         lockType[0].freePercent[2] = 20;     //20%
-        lockType[0].time[3] = 180;             
-        lockType[0].freePercent[3] = 20;     //20% 
-              
+        lockType[0].time[3] = 180;
+        lockType[0].freePercent[3] = 20;     //20%
+
         //Lock type 2
-        lockType[1].time[0] = 30;    
+        lockType[1].time[0] = 30;
         lockType[1].freePercent[0] = 25;     //25%
-        lockType[1].time[1] = 60;    
+        lockType[1].time[1] = 60;
         lockType[1].freePercent[1] = 25;     //25%
-        lockType[1].time[2] = 120;    
+        lockType[1].time[2] = 120;
         lockType[1].freePercent[2] = 25;     //25%
-        lockType[1].time[3] = 180;    
+        lockType[1].time[3] = 180;
         lockType[1].freePercent[3] = 25;     //25%
 
-        //Lock type 3    
-        lockType[2].time[0] = 180;    
+        //Lock type 3
+        lockType[2].time[0] = 180;
         lockType[2].freePercent[0] = 25;     //25%
-        lockType[2].time[1] = 360;    
+        lockType[2].time[1] = 360;
         lockType[2].freePercent[1] = 25;     //25%
-        lockType[2].time[2] = 540;    
+        lockType[2].time[2] = 540;
         lockType[2].freePercent[2] = 25;     //25%
-        lockType[2].time[3] = 720;    
+        lockType[2].time[3] = 720;
         lockType[2].freePercent[3] = 25;     //25%
 
         //init all lock data
-    }    
-    
+    }
+
+    /**
+    * @dev Gets the token name
+    * @return string representing the token name
+    */
+    function name() external view returns (string) {
+        return string(name_);
+    }
+
+    /**
+    * @dev Gets the token symbol
+    * @return string representing the token symbol
+    */
+    function symbol() external view returns (string) {
+        return string(symbol_);
+    }
+
     /**
      * Calculate how much tokens must be locked
      * return the amount of locked tokens
@@ -232,22 +204,22 @@ contract ioeXTokenERC20 is StandardToken, owned {
         uint256 lockTypeIndex;
         uint256 amountLockedTokens = 0;
         uint256 resultFreePercent = 0;
+        uint256 duration = 0;
         uint256 i;
-        uint256 duration;
- 
+
         lockTypeIndex = lockAccountInfo[account].lockType;
 
         if (lockTypeIndex >= 1) {
             if (lockTypeIndex <= LOCK_TYPE_MAX) {
                 lockTypeIndex = lockTypeIndex.sub(1);
                 for (i = 0; i < LOCK_STAGE_MAX; i++) {
-                    duration = lockType[lockTypeIndex].time[i] * 1 days;                                   
+                    duration = (lockType[lockTypeIndex].time[i]).mul(1 days);
                     if (lockAccountInfo[account].startTime.add(duration) >= now) {
                         resultFreePercent = resultFreePercent.add(lockType[lockTypeIndex].freePercent[i]);
                     }
                 }
             }
-            
+
             amountLockedTokens = (lockAccountInfo[account].initBalance.mul(resultFreePercent)).div(100);
 
             if (amountLockedTokens == 0){
@@ -270,12 +242,12 @@ contract ioeXTokenERC20 is StandardToken, owned {
             (lockAccountInfo[_to].initBalance == 0)) {
             require(_value <= balances[msg.sender]);
             require(_to != address(0));
-             
+
             //write data
             lockAccountInfo[_to].lockType = selectType;
             lockAccountInfo[_to].initBalance = _value;
             lockAccountInfo[_to].startTime = now;
-            emit SetlockData(_to,_value, lockAccountInfo[_to].lockType, lockAccountInfo[_to].startTime);
+            emit SetLockData(_to,_value, lockAccountInfo[_to].lockType, lockAccountInfo[_to].startTime);
             //write data
 
             balances[msg.sender] = balances[msg.sender].sub(_value);
@@ -306,8 +278,8 @@ contract ioeXTokenERC20 is StandardToken, owned {
 
         require(_value <= balances[msg.sender]);
         require(_to != address(0));
-        require(!frozenAccount[msg.sender]);                // Check if sender is frozen
-        require(!frozenAccount[_to]);                       // Check if recipient is frozen
+        require(!frozenAccount[msg.sender]);        // Check if sender is frozen
+        require(!frozenAccount[_to]);               // Check if recipient is frozen
 
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
@@ -322,10 +294,10 @@ contract ioeXTokenERC20 is StandardToken, owned {
         frozenAccount[target] = freeze;
         emit FrozenFunds(target, freeze);
     }
-    
+
     /**
      * Transfer tokens
-     * Lock time and token by lock_type 1  
+     * Lock time and token by lock_type 1
      *
      * Send `_value` tokens to `_to` from your account
      *
